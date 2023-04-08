43619d1624497bc342b900e7f0967e578b116d9c
5x12 <contact@awolf.io>
2023-04-08 22:41:58 +0300

    removing useless code

--- supplements/recsys/app/api.py
+++ supplements/recsys/app/api.py
@@ -1,26 +1,16 @@
 import json
-# from flask import Flask
-# from flask import request
 from inference import get_recommendations
-
-# app = Flask(__name__)
-# @app.route('/')
-# def index():
-#     name = request.args.get('name')
-#     print(name)
-#     responce = get_recommendations(646903)
-#     return json.dumps(responce)
-# app.run()
-
 from flask import Flask, request
+
+
 appFlask = Flask(__name__)
+
 @appFlask.route('/index')
 
 def access_param():
     id = request.args.get('id')
     responce = get_recommendations(id)
     return json.dumps(responce)
-    # return '''<h1>The source value is: {}</h1>'''.format(source)
 
 appFlask.run(debug=True, port=5000)
 
