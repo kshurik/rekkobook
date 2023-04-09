5c577759f4f4e5b841b58104dba89db16626f695
5x12 <contact@awolf.io>
2023-04-09 17:00:41 +0300

    adjusting api.py to look for 0.0.0.0

--- supplements/recsys/app/api.py
+++ supplements/recsys/app/api.py
@@ -12,6 +12,8 @@ def access_param():
     responce = get_recommendations(id)
     return json.dumps(responce)
 
-appFlask.run(debug=True, port=5000)
+appFlask.run(debug=True, host="0.0.0.0", port=5000)
 
 # http://127.0.0.1:5000/index?id=646321
+# http://127.0.0.1:5000/index?id=1047345
+
