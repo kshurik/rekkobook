6afb61e480ed37e0ca6295bad84976ee186e9296
5x12 <contact@awolf.io>
2023-04-10 13:05:22 +0300

    adjusting app/api.py

--- supplements/recsys/app/api.py
+++ supplements/recsys/app/api.py
@@ -3,16 +3,16 @@ from inference import get_recommendations
 from flask import Flask, request
 
 
-appFlask = Flask(__name__)
+app = Flask(__name__)
 
-@appFlask.route('/index')
+@app.route('/index')
 
 def access_param():
     id = request.args.get('id')
     responce = get_recommendations(id)
     return json.dumps(responce)
 
-appFlask.run(debug=True, host="0.0.0.0", port=5000)
+app.run(debug=True, host="0.0.0.0", port=5000)
 
 # http://127.0.0.1:5000/index?id=646321
 # http://127.0.0.1:5000/index?id=1047345
