efb2e38e711059dd44f2fc7d7c1f39afb45ffb13
5x12 <contact@awolf.io>
2023-04-24 20:58:29 +0300

    adding last changes

--- supplements/recsys/app/api.py
+++ supplements/recsys/app/api.py
@@ -1,14 +1,15 @@
 import json
-from inference import get_recommendations
+
 from flask import Flask, request
+
+from inference import get_recommendations
 
 
 app = Flask(__name__)
 
 @app.route('/index')
-
 def access_param():
-    id = request.args.get('id')
+    id = request.args.get('id') #646321
     responce = get_recommendations(id)
     return json.dumps(responce)
 
@@ -16,4 +17,4 @@ app.run(debug=True, host="0.0.0.0", port=5000)
 
 # http://127.0.0.1:5000/index?id=646321
 # http://127.0.0.1:5000/index?id=1047345
-
+# CHECK THAT IT WORKS!
