33ef2a90ffdae27cde9d13a98a2dd01d221ed51d
Shuhrat Khalilbekov <khalilbekov92@gmail.com>
2023-05-09 13:32:05 +0300

    updated api.py

--- supplements/recsys/api.py
+++ supplements/recsys/api.py
@@ -27,6 +27,4 @@ def run():
     return json.dumps(response, cls = JsonEncoder)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port = 8000)
-
-# http://127.0.0.1:8000/get_recommendation?id=646321
+    app.run(debug=True, host="0.0.0.0", port=8000)
